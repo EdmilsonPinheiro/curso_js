165e01c41ff98c8fc64565b4e1cbe7f1f93f7603
Edmilson Junior <edmilson.junior@cdcbank.com.br>
2025-05-12 17:56:17 -0300

    feat
    novos exercicios.

--- js/scripts.js
+++ js/scripts.js
@@ -559,16 +559,84 @@ while (valor_teste < 10){
 } */
 
 //Aula 30 Formulários
+
+/* //select box
+document.getElementById("mostrar_opcao").onclick = function () {
+
+    var campo_select = document.getElementById("options");
+    var indice_selecionado = campo_select.options.selectedIndex;
+    var valor_selecionado = campo_select.options[indice_selecionado].innerHTML;
+    document.getElementById("opcao_selecionada").innerHTML = valor_selecionado;
+
+};
+
+// radio buttons
+
+document.getElementById("mostrar_radio").onclick = function (){
+
+    var radio = document.getElementsByName("genero");
+    var radio_selected;
+
+    for (var a = 0; a < radio.length;a++){
+        if (radio[a].checked)
+            radio_selected = radio[a].value;
+    }
+    document.getElementById("radio_selecionado").innerHTML = radio_selected;
+};
+
+// check box
+
+document.getElementById("mostrar_check").onclick = function (){
+
+    var check = document.getElementsByName("interesse");
+    document.getElementById("check_selecionado").innerHTML = "";
+
+    for(var b = 0; b < check.length; b++){
+        if (check[b].checked){
+            document.getElementById("check_selecionado").innerHTML += '<li>' + check[b].value + '</li>';
+        }
+    }
+};
+ */
+
+/* // data
+
+document.getElementById("mostrar_data").onclick = function () {
+
+    var data_select = document.getElementById("data_evento").value;
+    var data_completa = new Date(data_select);
+    document.getElementById("data_selecionada").innerHTML = data_completa;
+
+};
+ */
+
+// Aula 31 onchange
 /* 
-document.getElementById("mostrar_opcao").onclick = function(){
+document.getElementById("escolaridade").onchange = function () {
 
-}; */
+    var campo_select = document.getElementById("escolaridade");
+    var indice_selecionado = campo_select.options.selectedIndex;
+    var valor_selecionado = campo_select.options[indice_selecionado].innerHTML;
+    document.getElementById("escolaridade_selecionada").innerHTML = valor_selecionado;
 
+};
 
+var check = document.getElementsByName("lanche");
 
+for (var a = 0;  a < check.length; a++) {
 
+    check[a].onchange = function () {
 
-
+        document.getElementById("check_selecionado").innerHTML = "";
+        
+        for (var b = 0;  b < check.length; b++) {
+            
+            if (check[b].checked) {
+                document.getElementById("check_selecionado").innerHTML += '<li>' + check[b].value + '</li>';
+            }
+        }
+    }  
+} */
 
 
 /* //AULA 35
